79e82242b82fb3eeef8dff1ff079982b8233c8a1
manish-mdr <maanish123mdr@gmail.com>
2025-11-02 22:29:20 +0900

    route group

--- app/layout.js
+++ app/layout.js
@@ -1,12 +1,7 @@
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
-        <header>Header</header>
-        {children}
-
-        <footer>Footer</footer>
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
